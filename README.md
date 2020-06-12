878427630ceedfe524ba9606ec2c3a291336c48d
Albert Attard <albert.attard@thoughtworks.com>
2020-06-12 15:32:11 +0200

    Fix style

--- README.md
+++ README.md
@@ -3,3 +3,35 @@
 Boot Camp for programmers interested to learn Spring and Spring Boot.
 
 Please visit: [https://albertattard.github.io/spring-boot-camp/](https://albertattard.github.io/spring-boot-camp/) to see the Spring Boot Camp Github Pages.
+
+## Useful commands
+
+1. Install the Javascript libraries
+
+    ```bash
+    $ npm install
+    ```
+
+1. Install Ruby Gems
+
+    ```bash
+    $ bundle install
+    ```
+
+1. Format the files
+
+    ```bash
+    $ npm run format
+    ```
+
+1. Ensure that the files are well formatted
+
+    ```bash
+    $ npm test
+    ```
+
+1. Start the local server
+
+    ```bash
+    $ bundle exec jekyll serve
+    ```
--- assets/js/copy-code.js
+++ assets/js/copy-code.js
@@ -1,23 +1,25 @@
-const copyCode = (clickEvent) => {
-    console.log("clicked...")
-    // const copyCodeButton = clickEvent.target;
-    // const tempTextArea = document.createElement('textarea');
-    // tempTextArea.textContent = copyCodeButton.getAttribute('data-code');
-    // document.body.appendChild(tempTextArea);
-    //
-    // const selection = document.getSelection();
-    // selection.removeAllRanges();
-    // tempTextArea.select();
-    // document.execCommand('copy');
-    // selection.removeAllRanges();
-    // document.body.removeChild(tempTextArea);
-    //
-    // copyCodeButton.classList.add('copied');
-    // setTimeout(() => {
-    //     copyCodeButton.classList.remove('copied');
-    // }, 2000);
-};
+function getTextContent(copyCodeButton) {
+  const dataCode = copyCodeButton.getAttribute("data-code")
 
-// document.querySelectorAll('.copy-code-button').forEach((copyCodeButton) => {
-//     copyCodeButton.addEventListener('click', copyCode);
-// });
+  // Did not find another way to detect line breaks
+  // than converting them to <br>s and back again
+  return dataCode.replace("<br /> ", "\n")
+}
+
+const copyCode = (copyCodeButton) => {
+  const tempTextArea = document.createElement("textarea")
+  tempTextArea.textContent = getTextContent(copyCodeButton)
+  document.body.appendChild(tempTextArea)
+
+  const selection = document.getSelection()
+  selection.removeAllRanges()
+  tempTextArea.select()
+  document.execCommand("copy")
+  selection.removeAllRanges()
+  document.body.removeChild(tempTextArea)
+
+  copyCodeButton.classList.add("copied")
+  setTimeout(() => {
+    copyCodeButton.classList.remove("copied")
+  }, 2000)
+}
